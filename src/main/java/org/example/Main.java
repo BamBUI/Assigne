fce4ca786eacd4336c8e2ada1e33cc4ffbe395d7
p.mironov <p.mironov@kg.ru>
2024-10-03 22:47:06 +0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args)
+    throws java.io.IOException {
+        String name;
+        name = System.in.read();
+        System.out.println("Hello world from " + name);
+    }
+}
