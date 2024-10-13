bd53b0caf53a7759c26e171b7ee857373c938f0b
p.mironov <p.mironov@kg.ru>
2024-10-13 11:23:51 +0400

    Comment

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args)
     throws java.io.IOException {
+        //Comment
         String name;
         name = String.valueOf(System.in.read());
         System.out.println("Hello world from " + name + ".");
